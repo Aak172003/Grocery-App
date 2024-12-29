323f3e90048b45912de5cdf7fddb709ff15285db
Aakashmoondive502 <aakash.prajapati@moondive.co>
2024-12-29 18:48:41 +0530

    setup-geo-location-read-images-file-setup-stack-navigation-setup-zustand-mmvk

--- App.tsx
+++ App.tsx
@@ -1,16 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Navigation from '@navigation/Navigation'
-import SplashScreen from '@features/auth/SplashScreen'
 // Now we are using absolute path
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-      <Navigation />
-      <SplashScreen />
-    </View>
+    <Navigation />
   )
 }
 
---
+++ src/features/auth/CustomerLogin.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React, { FC } from 'react'
+
+const CustomerLogin: FC = () => {
+    return (
+        <View>
+            <Text>CustomerLogin</Text>
+        </View>
+    )
+}
+
+export default CustomerLogin
